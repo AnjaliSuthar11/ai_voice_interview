b6807ea0801bd3e0a24dd46382a3bb3be2a71087
Anjali Suthar <anjalisuthar114@gmail.com>
2025-04-27 18:28:27 +0530

    added json file

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
-import{vapi} from '@/lib/vapi.sdk'
+import {vapi} from '@/lib/vapi.sdk'
 
 enum CallStatus {
     INACTIVE='INACTIVE',
